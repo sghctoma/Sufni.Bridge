361ca36384c4472c275c264f237e80972defdb3f
sghctoma <sghctoma@gmail.com>
2023-07-25 16:43:43 +0200

    Use dependency injection for services

--- Sufni.Bridge/Sufni.Bridge/App.axaml.cs
+++ Sufni.Bridge/Sufni.Bridge/App.axaml.cs
@@ -1,36 +1,48 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using Sufni.Bridge.Services;
 using Sufni.Bridge.ViewModels;
 using Sufni.Bridge.Views;
+using System;
 
-namespace Sufni.Bridge
+namespace Sufni.Bridge;
+
+public partial class App : Application
 {
-    public partial class App : Application
+    public readonly IServiceProvider AppServiceProvider;
+
+    public App(IServiceCollection services)
     {
-        public override void Initialize()
+        services.AddSingleton<IHttpApiService, HttpApiService>();
+        AppServiceProvider = services.BuildServiceProvider();
+    }
+
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        var viewModel = ActivatorUtilities.CreateInstance<MainViewModel>(AppServiceProvider);
+
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            AvaloniaXamlLoader.Load(this);
+            desktop.MainWindow = new MainWindow
+            {
+                DataContext = viewModel
+            };
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        {
+            singleViewPlatform.MainView = new MainView
+            {
+                DataContext = viewModel
+            };
         }
 
-        public override void OnFrameworkInitializationCompleted()
-        {
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = new MainViewModel()
-                };
-            }
-            else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
-            {
-                singleViewPlatform.MainView = new MainView
-                {
-                    DataContext = new MainViewModel()
-                };
-            }
-
-            base.OnFrameworkInitializationCompleted();
-        }
+        base.OnFrameworkInitializationCompleted();
     }
 }

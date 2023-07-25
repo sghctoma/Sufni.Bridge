576ebe1a1f5fe8420a24250204e8dca2f6e46934
sghctoma <sghctoma@gmail.com>
2023-07-25 19:28:21 +0200

    Create main layout

--- Sufni.Bridge/Sufni.Bridge/ViewModels/MainViewModel.cs
+++ Sufni.Bridge/Sufni.Bridge/ViewModels/MainViewModel.cs
@@ -1,7 +1,35 @@
-﻿namespace Sufni.Bridge.ViewModels
+﻿using Sufni.Bridge.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+
+namespace Sufni.Bridge.ViewModels;
+public class MainViewModel : ViewModelBase
 {
-    public class MainViewModel : ViewModelBase
+    public ObservableCollection<TelemetrySession>? TelemetryFiles { get; set; }
+
+    public bool IsRegistered { get; set; }
+
+    public MainViewModel()
     {
-        public string Greeting => "Welcome to Avalonia!";
+        IsRegistered = true;
+
+        var drives = DriveInfo.GetDrives()
+            .Where(drive => drive is
+            {
+                IsReady: true,
+                DriveType: DriveType.Removable,
+                DriveFormat: "FAT32"
+            } && File.Exists($"{drive.RootDirectory}/.boardid"))
+            .Select(d => new TelemetryDataStore(d.VolumeLabel, d.RootDirectory))
+            .ToList();
+
+        List<TelemetrySession> sessions = new List<TelemetrySession>();
+        foreach (var drive in drives)
+        {
+            sessions.AddRange(drive.Files);
+        }
+        TelemetryFiles = new ObservableCollection<TelemetrySession>(sessions);
     }
 }

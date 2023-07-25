627ceec52b3eb48907519cd5ba6325558d3665e1
sghctoma <sghctoma@gmail.com>
2023-07-25 19:23:10 +0200

    Add registration window

---
+++ Sufni.Bridge/Sufni.Bridge/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sufni.Bridge.ViewModels;
+
+public class RegisterViewModel : ViewModelBase
+{
+    [Required]
+    [Url]
+    public string? ServerUrl { get; set; }
+    [Required]
+    public string? Username { get; set; }
+    [Required]
+    public string? Password { get; set; }
+
+    private void DoRegister()
+    {
+        
+    }
+}
---
+++ Sufni.Bridge/Sufni.Bridge/Views/RegisterView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Sufni.Bridge.Views
+{
+    public partial class RegisterView : UserControl
+    {
+        public RegisterView()
+        {
+            InitializeComponent();
+        }
+    }
+}

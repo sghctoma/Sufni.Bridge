7678e6ec3bf0d9f4f2952681fa21dbb833bdb065
sghctoma <sghctoma@gmail.com>
2023-07-25 19:28:05 +0200

    Add extension methods to access injected services

--- Sufni.Bridge/Sufni.Bridge.Desktop/Program.cs
+++ Sufni.Bridge/Sufni.Bridge.Desktop/Program.cs
@@ -1,10 +1,20 @@
 ﻿using Avalonia;
+using Microsoft.Extensions.DependencyInjection;
+using SecureStorage;
 using System;
 
-namespace Sufni.Bridge.Desktop
+namespace Sufni.Bridge.Windows
 {
     internal class Program
     {
+        private static readonly IServiceCollection Services;
+
+        static Program()
+        {
+            Services = new ServiceCollection();
+            Services.AddSingleton<ISecureStorage, SecureStorage.SecureStorage>();
+        }
+
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
@@ -14,7 +24,7 @@ namespace Sufni.Bridge.Desktop
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
+            => AppBuilder.Configure( () => new App(Services))
                 .UsePlatformDetect()
                 .WithInterFont()
                 .LogToTrace();
---
+++ Sufni.Bridge/Sufni.Bridge/ViewModelBaseExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Microsoft.Extensions.DependencyInjection;
+using Sufni.Bridge.ViewModels;
+using System;
+
+namespace Sufni.Bridge;
+
+public static class ViewModelBaseExtensions
+{
+    public static IServiceProvider GetServiceProvider(this ViewModelBase vm)
+    {
+        if (Application.Current == null)
+        {
+            throw new Exception("Expected service provider missing");
+        }
+        return (Application.Current as App)!.AppServiceProvider;
+    }
+
+    public static T CreateInstance<T>(this ViewModelBase vm)
+    {
+        return ActivatorUtilities.CreateInstance<T>(vm.GetServiceProvider());
+    }
+
+    public static T GetServiceOrCreateInstance<T>(this ViewModelBase vm)
+    {
+        return ActivatorUtilities.GetServiceOrCreateInstance<T>(vm.GetServiceProvider());
+    }
+}

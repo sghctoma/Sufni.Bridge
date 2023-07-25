1f3ff64405cd742fb4b61561be97a42e38319277
sghctoma <sghctoma@gmail.com>
2023-07-25 16:42:07 +0200

    Add HTTP API service

---
+++ Sufni.Bridge/Sufni.Bridge/Services/HttpApiService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace Sufni.Bridge.Services;
+
+internal class HttpApiService : IHttpApiService
+{
+    public string? ServerUrl
+    {
+        get
+        {
+            return _client.BaseAddress?.ToString();
+        }
+
+        set
+        {
+            if (value != null)
+            {
+                _client.BaseAddress = new Uri(value);
+            }
+        }
+    }
+
+    private string? _sessionToken;
+
+    private readonly HttpClient _client = new();
+
+    public async Task<bool> IsRegisteredAsync()
+    {
+        if (_sessionToken == null || ServerUrl == null)
+        {
+            return false;
+        }
+
+        var response = await _client.GetAsync("/auth/user");
+        return response.StatusCode == System.Net.HttpStatusCode.OK;
+    }
+
+    public async Task<JsonObject?> RegisterAsync(string url, string username, string password)
+    {
+        _client.BaseAddress = new Uri(url);
+
+        var jsonObject = new JsonObject();
+        jsonObject["username"] = username;
+        jsonObject["password"] = password;
+        var content = new StringContent(jsonObject.ToString(), Encoding.UTF8, "application/json");
+        var response = await _client.PostAsync("/auth/login", content);
+
+        return await response.Content.ReadFromJsonAsync<JsonObject>();
+    }
+}
---
+++ Sufni.Bridge/Sufni.Bridge/Services/IHttpApiService.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Controls.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace Sufni.Bridge.Services;
+
+internal interface IHttpApiService
+{ 
+    public Task<bool> IsRegisteredAsync();
+    public Task<JsonObject?> RegisterAsync(string url, string username, string password);
+}

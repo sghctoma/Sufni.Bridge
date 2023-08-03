b1b5c1bf32bf3f23102ba6ce73ea55cd70e3b264
sghctoma <sghctoma@gmail.com>
2023-08-03 16:05:15 +0200

    Indicate if there's no bike setup for selected DAQ unit

--- Sufni.Bridge/Sufni.Bridge/ViewModels/MainViewModel.cs
+++ Sufni.Bridge/Sufni.Bridge/ViewModels/MainViewModel.cs
@@ -5,9 +5,42 @@ namespace Sufni.Bridge.ViewModels;
 public partial class MainViewModel : ViewModelBase
 {
     #region Observable properties
+    
+    [ObservableProperty] private ImportSessionsViewModel importSessionsPage = new();
+    [ObservableProperty] private SettingsViewModel settingsPage = new();
+    [ObservableProperty] private int selectedIndex;
+    [ObservableProperty] private bool isImportSessionsPageSelected = true;
+    
+    #endregion
 
-    [ObservableProperty] private ViewModelBase importSessionsPage = new ImportSessionsViewModel();
-    [ObservableProperty] private ViewModelBase settingsPage = new SettingsViewModel();
+    #region Property change handlers
+
+    partial void OnSelectedIndexChanged(int value)
+    {
+        IsImportSessionsPageSelected = value == (int)PageIndices.ImportSessions;
+    }
+
+    #endregion
+    
+    #region Private fields
+    
+    private enum PageIndices
+    {
+        ImportSessions = 0,
+        Settings = 1,
+        Linkages = 2,
+        Calibrations = 3,
+        BikeSetups = 4,
+    }
+
+    #endregion
+    
+    #region Constructors
+
+    public MainViewModel()
+    {
+        SelectedIndex = SettingsPage.IsRegistered ? (int)PageIndices.ImportSessions : (int)PageIndices.Settings;
+    }
 
     #endregion
 }
--- Sufni.Bridge/Sufni.Bridge/Views/MainView.axaml.cs
+++ Sufni.Bridge/Sufni.Bridge/Views/MainView.axaml.cs
@@ -1,4 +1,6 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace Sufni.Bridge.Views
 {
@@ -7,6 +9,15 @@ namespace Sufni.Bridge.Views
         public MainView()
         {
             InitializeComponent();
+        }
+
+        protected override void OnSizeChanged(SizeChangedEventArgs e)
+        {
+            base.OnSizeChanged(e);
+            
+            var rm = BikeSetupsTabItem.Bounds.Width / 2 - 20;
+            if (double.IsNaN(rm)) return;
+            MissingSetupNotification.Margin = new Thickness(0, 0, rm, 35);
         }
     }
 }

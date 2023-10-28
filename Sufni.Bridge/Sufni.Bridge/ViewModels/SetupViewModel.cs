9124f09691136a4e2f9088e7b8260a435f6d9b67
sghctoma <sghctoma@gmail.com>
2023-10-28 22:36:54 +0200

    Indicate unsaved changes on Setups page

--- Sufni.Bridge/Sufni.Bridge/ViewModels/SetupViewModel.cs
+++ Sufni.Bridge/Sufni.Bridge/ViewModels/SetupViewModel.cs
@@ -1,27 +1,76 @@
 using System.Collections.ObjectModel;
 using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Sufni.Bridge.Models;
 
 namespace Sufni.Bridge.ViewModels;
 
 public partial class SetupViewModel : ViewModelBase
 {
-    [ObservableProperty] private string? name;
-    public ObservableCollection<Linkage> Linkages { get; }
-    public ObservableCollection<Calibration> Calibrations { get; }
-    [ObservableProperty] private Linkage? selectedLinkage;
-    [ObservableProperty] private Calibration? selectedFrontCalibration;
-    [ObservableProperty] private Calibration? selectedRearCalibration;
+    #region Observable properties
+
+    [ObservableProperty] private Setup setup;
     
-    public SetupViewModel(Setup setup, ObservableCollection<Linkage> linkages, ObservableCollection<Calibration> calibrations)
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(SaveCommand))]
+    private bool isDirty;
+
+    public ObservableCollection<LinkageViewModel> Linkages { get; }
+    public ObservableCollection<CalibrationViewModel> Calibrations { get; }
+    
+    [ObservableProperty] private LinkageViewModel? selectedLinkage;
+    [ObservableProperty] private CalibrationViewModel? selectedFrontCalibration;
+    [ObservableProperty] private CalibrationViewModel? selectedRearCalibration;
+    
+    #endregion
+
+    #region Property change handlers
+
+    partial void OnSelectedLinkageChanged(LinkageViewModel? value)
     {
-        Name = setup.Name;
+        IsDirty = value == null || value.Linkage.Id != Setup.LinkageId;
+    }
+
+    partial void OnSelectedFrontCalibrationChanged(CalibrationViewModel? value)
+    {
+        IsDirty = value == null || value.Calibration.Id != Setup.FrontCalibrationId;
+    }
+
+    partial void OnSelectedRearCalibrationChanged(CalibrationViewModel? value)
+    {
+        IsDirty = value == null || value.Calibration.Id != Setup.RearCalibrationId;
+    }
+
+    #endregion
+
+    #region Constructors
+
+    public SetupViewModel(Setup setup, ObservableCollection<LinkageViewModel> linkages, ObservableCollection<CalibrationViewModel> calibrations)
+    {
+        Setup = setup;
         Linkages = linkages;
         Calibrations = calibrations;
 
-        SelectedLinkage = Linkages.First(l => l.Id == setup.LinkageId);
-        SelectedFrontCalibration = Calibrations.First(c => c.Id == setup.FrontCalibrationId);
-        SelectedRearCalibration = Calibrations.First(c => c.Id == setup.RearCalibrationId);
+        SelectedLinkage = Linkages.First(l => l.Linkage.Id == setup.LinkageId);
+        SelectedFrontCalibration = Calibrations.First(c => c.Calibration.Id == setup.FrontCalibrationId);
+        SelectedRearCalibration = Calibrations.First(c => c.Calibration.Id == setup.RearCalibrationId);
     }
+
+    #endregion
+
+    #region Commands
+
+    private bool CanSave()
+    {
+        return IsDirty;
+    }
+
+    [RelayCommand(CanExecute = nameof(CanSave))]
+    private void Save()
+    {
+        
+    }
+
+    #endregion
 }
--- Sufni.Bridge/Sufni.Bridge/Views/MainView.axaml.cs
+++ Sufni.Bridge/Sufni.Bridge/Views/MainView.axaml.cs
@@ -1,9 +1,32 @@
+using System;
+using System.Globalization;
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Input;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
 
 namespace Sufni.Bridge.Views
 {
+    public class HeaderColorConverter : IValueConverter
+    {
+        public static readonly HeaderColorConverter Instance = new();
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool isDirty)
+            {
+                return isDirty ? Colors.RosyBrown : Colors.Transparent;
+            }
+            return new BindingNotification(new InvalidCastException(), BindingErrorType.Error);
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+    
     public partial class MainView : UserControl
     {
         public MainView()

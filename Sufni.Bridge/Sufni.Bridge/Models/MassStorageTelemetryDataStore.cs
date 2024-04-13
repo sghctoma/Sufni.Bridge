f5546ed0670423808dca92d1df1f85a9bde8c8ad
sghctoma <sghctoma@gmail.com>
2024-04-13 08:26:37 +0200

    User BOARDID everywhere instead of .boardid

--- Sufni.Bridge/Sufni.Bridge/Models/MassStorageTelemetryDataStore.cs
+++ Sufni.Bridge/Sufni.Bridge/Models/MassStorageTelemetryDataStore.cs
@@ -26,7 +26,7 @@ public class MassStorageTelemetryDataStore : ITelemetryDataStore
     {
         DriveInfo = driveInfo;
         Name = $"{driveInfo.VolumeLabel} ({DriveInfo.RootDirectory.Name})";
-        BoardId = File.ReadAllText($"{DriveInfo.RootDirectory.FullName}/.boardid").ToLower();
+        BoardId = File.ReadAllText($"{DriveInfo.RootDirectory.FullName}/BOARDID").ToLower();
 
         if (!Directory.Exists($"{DriveInfo.RootDirectory.FullName}/uploaded"))
             Directory.CreateDirectory($"{DriveInfo.RootDirectory.FullName}/uploaded");

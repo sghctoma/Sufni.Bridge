f9ef42ecaa71c77191b86c8ef0ff1c94031a9fdf
sghctoma <sghctoma@gmail.com>
2024-05-11 15:43:16 +0200

    Move the hamburger menu to the main menu strip

--- Sufni.Bridge/Sufni.Bridge/Views/MainPagesView.axaml.cs
+++ Sufni.Bridge/Sufni.Bridge/Views/MainPagesView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
 
 namespace Sufni.Bridge.Views;
 
@@ -7,5 +9,20 @@ public partial class MainPagesView : UserControl
     public MainPagesView()
     {
         InitializeComponent();
+    }
+
+    private void MenuTabItem_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (sender is Control ctl)
+        {
+            FlyoutBase.ShowAttachedFlyout(ctl);
+        }
+
+        e.Handled = true;
+    }
+
+    private void MenuTabItem_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
+    {
+        e.Handled = true;
     }
 }

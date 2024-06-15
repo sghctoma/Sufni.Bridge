04035c2f0c639c2fff44bb8895a5b6f79a26ac6d
sghctoma <sghctoma@gmail.com>
2024-06-15 06:18:28 +0200

    Make SessionView's tab pages individual controls

---
+++ Sufni.Bridge/Sufni.Bridge/Views/SessionPages/DamperPageView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Sufni.Bridge.Views.SessionPages;
+
+public partial class DamperPageView : UserControl
+{
+    public DamperPageView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Sufni.Bridge/Sufni.Bridge/Views/SessionPages/NotesPageView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Sufni.Bridge.Views.SessionPages;
+
+public partial class NotesPageView : UserControl
+{
+    public NotesPageView()
+    {
+        InitializeComponent();
+    }
+}

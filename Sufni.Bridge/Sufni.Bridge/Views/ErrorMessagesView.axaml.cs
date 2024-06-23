1c5ca3de80966332ce0bb1487d5d675852c53f9d
sghctoma <sghctoma@gmail.com>
2024-06-23 08:35:54 +0200

    Convert error/notification bars to UserControls
    There was no real reason for these to be TemplatedControls, and
    converting them to UserControls made the code simpler.

---
+++ Sufni.Bridge/Sufni.Bridge/Views/ErrorMessagesView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Sufni.Bridge.Views
+{
+    public partial class ErrorMessagesView : UserControl
+    {
+        public ErrorMessagesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Sufni.Bridge/Sufni.Bridge/Views/NotificationsView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Sufni.Bridge.Views
+{
+    public partial class NotificationsView : UserControl
+    {
+        public NotificationsView()
+        {
+            InitializeComponent();
+        }
+    }
+}

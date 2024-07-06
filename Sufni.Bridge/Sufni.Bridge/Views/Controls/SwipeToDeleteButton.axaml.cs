5910353dd030cae335f6d64fb1a687508262da76
sghctoma <sghctoma@gmail.com>
2024-07-06 13:41:41 +0200

    Restyle item lists

--- Sufni.Bridge/Sufni.Bridge/Views/Controls/SwipeToDeleteButton.axaml.cs
+++ Sufni.Bridge/Sufni.Bridge/Views/Controls/SwipeToDeleteButton.axaml.cs
@@ -5,6 +5,7 @@ using Avalonia.Controls;
 using Avalonia.Controls.Presenters;
 using Avalonia.Labs.Controls;
 using Avalonia.Media.Transformation;
+using Avalonia.Svg.Skia;
 using HapticFeedback;
 using Microsoft.Extensions.DependencyInjection;
 using Sufni.Bridge.ViewModels.Items;
@@ -22,26 +23,35 @@ public partial class SwipeToDeleteButton : UserControl
         SwipeButton.Children[2].PropertyChanged += async (s, e) =>
         {
             var deleteButton = ((ContentPresenter)SwipeButton.Children[1]).Content as Button;
-            if (e.Property.Name == "RenderTransform" &&
-                e.NewValue is TransformOperations ops &&
-                deleteButton!.Command is not null &&
-                deleteButton!.Command!.CanExecute(false))
+
+            if (e.Property.Name == "RenderTransform" && e.NewValue is TransformOperations ops)
             {
                 var offset = ops.Operations[0].Matrix.M31;
+                var progress = offset / 64.0; // 64 = deleteButton.Width
+                deleteButton!.Padding = new Thickness(Math.Min(24.0, 24.0 * progress), 0, 0, 0); // 24 = (deleteButton.Width - width(svg)) / 2.0
+
+                if (deleteButton!.Command is null || !deleteButton!.Command!.CanExecute(false))
+                {
+                    return;
+                }
+
                 var trashcan = deleteButton!.Content as Image;
-                trashcan!.Opacity = Math.Min(1.0, offset / 64.0);
+                trashcan!.Opacity = animationPlayed ? 1.0 : Math.Min(0.5, progress / 2.0);
 
                 if (offset > deleteButton.Width && !animationPlayed)
                 {
                     hapticFeedback?.LongPress();
+                    deleteButton.SetCurrentValue(Avalonia.Svg.Skia.Svg.CssProperty, ".default { fill: #bf312d; }");
 
                     animationPlayed = true;
                     var animation = Resources["ImageSizeAnimation"] as Animation;
                     await animation!.RunAsync(trashcan);
                 }
-                if (offset < deleteButton.Width) 
+                if (offset < deleteButton.Width && animationPlayed)
                 {
                     animationPlayed = false;
+                    trashcan!.Opacity = 0.5;
+                    deleteButton.SetCurrentValue(Avalonia.Svg.Skia.Svg.CssProperty, ".default { fill: #f0f0f0; }");
                 }
             }
         };
